d5e009fa37f81b3aa58c9df0ed2bac49c6861164
Dre <andrefellone2004@gmail.com>
2025-09-20 17:39:38 +0800

    Added Input Field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
+import "./InputField.css";
 
 function InputField() {
   return (
     <>
-      <form action="#" className="frm_container">
-        <label htmlFor="new_item">New Item</label>
-        <input type="text" id="new_item"/>
-
-        <button id="btn_add">
+      <form action="#" className="input_field">
+        <h1>To-Do List</h1>
+        <input type="text" placeholder="Enter a task" className="input_box" />
+        <button className="btn_add" type="submit">
           Add
         </button>
       </form>
